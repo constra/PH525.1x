1032c55e1002a2d2a7d760588ff4ecb3c82c241f
Sun Mai <sun.mai@outlook.com>
2015-02-16 13:58:06 +0100

    null hyp

--- probability.R
+++ probability.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 ### Probability
 dat <- read.csv("femaleMiceWeights.csv")
 mean(dat[13:24,2]) - mean(dat[1:12,2])
@@ -10,3 +12,41 @@ highfat = s[["hf"]]
 highfat
 
 sample(highfat,6)
+
+highfat >30
+as.numeric(highfat >30)
+sum(highfat >30)
+
+sum(highfat >30)/length(highfat)
+
+population <- read.csv("femaleControlsPopulation.csv")
+≈
+
+control <- sample(population[,1,12])
+mean(control)
+
+n <- 100000
+null <- vector("numeric",n)
+for(i in 1:n){
+        control <- sample(population[,1],12)
+        treatment <- sample(population[,1],12)
+        null[i] <- mean(treatment) - mean(control)
+}
+
+diff <- mean(dat[13:24,2]) - mean(dat[1:12,2])
+mean(null>diff)
+
+
+mean(population[,1])
+population <- population[,1]
+sampleMean <- replicate(10000, mean(sample(population, 12)))
+plot(sampleMean)
+hist(sampleMean)
+null <- replicate(10000,mean(sample(population,12))-mean(sample(population,12)))
+plot(null)
+hist(null)
+abline(v=diff,col="red")
+
+# using ggplot2
+q<-qplot(null,binwith=.3)
+q+geom_vline(xintercept=diff,color="red")
